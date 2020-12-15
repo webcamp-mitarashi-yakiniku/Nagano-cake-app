2d9bc208cd0f0a7f6addc8d44cc38c72b507b2a3
yamoritaketo <ymymz87@icloud.com>
2020-12-15 13:54:42 +0900

    [add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :admins, path: 'admin',controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  devise_for :customers, path: '',controllers: {
+    sessions: 'customer/sessions',
+    registrations: 'customer/registrations',
+    passwords: 'customer/passwords'
+  }
+
+  namespace :customer do
+    get 'homes/top'
+    get 'homes/about'
+  end
+
+
 end
