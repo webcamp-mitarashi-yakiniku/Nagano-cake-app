aa3298ddb730a535285ca8df31a3e807e050ee10
Yuta Nakano <nakano.yuta252@gmail.com>
2020-12-24 18:55:45 +0900

    [fix]画像アップロードプレビューのバグ修正及びアップロード画像の表示体裁修正

--- app/assets/javascripts/image_uplode.js
+++ app/assets/javascripts/image_uplode.js
@@ -1,17 +1,18 @@
-$(function () {
-  // inputのidから情報の取得
-  $('#product-image').on('change', function (e) {
-  // ここから既存の画像のurlの取得
+document.addEventListener('turbolinks:load', function(){
+    // ここにjQueryの処理を記述する
+    // inputのidから情報の取得
+    $('#product-image').on('change', function (e) {
+    // ここから既存の画像のurlの取得
     console.log("image upload is clicked")
     var reader = new FileReader();
     reader.onload = function (e) {
         $("#image").attr('src', e.target.result);
     }
-  // ここまで
+    // ここまで
     reader.readAsDataURL(e.target.files[0]); //取得したurlにアップロード画像のurlを挿入
-  });
+    });
 
-  $('#product-img').on('change', function (e) {
+    $('#product-img').on('change', function (e) {
     // ここから既存の画像のurlの取得
     console.log("image upload is clicked")
     var reader = new FileReader();
@@ -20,5 +21,5 @@ $(function () {
     }
     // ここまで
     reader.readAsDataURL(e.target.files[0]); //取得したurlにアップロード画像のurlを挿入
-  });
-});
+    });
+})
